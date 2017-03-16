54b66a6521ad935909ada7b7db3dc511cc13db78
leonliu06 <leonliu06@gmail.com>
2017-03-16 21:58:57 +0800

    init repo

---
+++ src/biz/superalloy/MyServlet.java
@@ -0,0 +1,93 @@
+package biz.superalloy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Map.Entry;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@WebServlet(name = "MyServlet", urlPatterns = { "/my" })
+public class MyServlet implements Servlet{
+	
+	private transient ServletConfig servletConfig;
+	
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		this.servletConfig = servletConfig;
+		
+		//org.apache.catalina.core.StandardContext
+		
+		
+		
+	}
+	
+	@Override
+	public ServletConfig getServletConfig(){
+		return servletConfig;
+	}
+	
+	@Override
+	public String getServletInfo(){
+		return "My Servlet";
+	}
+	
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		
+		System.out.println(this.servletConfig.getInitParameter("name"));
+		
+		Enumeration<String> sEnumeration = servletConfig.getInitParameterNames();
+		
+		while(sEnumeration.hasMoreElements()){
+			System.out.println(sEnumeration.nextElement());
+		}
+		
+		System.out.println("servletConfig.getServletContext() = " + servletConfig.getServletContext());
+		
+		System.out.println(request.getContentLength());
+		System.out.println(request.getContentType());
+		System.out.println(request.getParameterMap());
+		System.out.println(request.getProtocol());
+		System.out.println(request.getCharacterEncoding());
+		System.out.println(request.getLocalAddr());
+		System.out.println(request.getLocalName());
+		System.out.println(request.getLocalPort());
+		System.out.println(request.getServletContext());
+		System.out.println(request.getParameter("id"));
+		System.out.println(request.getParameter("name"));
+		System.out.println(request.getParameter("value"));
+		//System.out.println(request.getParameter("value").equals(""));
+		for(Entry<String, String[]> entry : request.getParameterMap().entrySet()){
+			System.out.println(entry.getKey());
+			for(String v : entry.getValue()){
+				System.out.print(v + " ");
+			}
+			System.out.println();
+		}
+		
+		
+		HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+		System.out.println(httpServletRequest.getMethod());
+		
+		String servletName = servletConfig.getServletName();
+		
+		//String initParameter = servletConfig.getInitParameter(name)
+		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.print("<html><head></head>" + "<body>Hello from " + servletName + "<script>alert('xxxx')</script>" + "</body></html>");
+	}
+	
+	@Override
+	public void destroy(){
+		
+	}
+
+}
---
+++ src/biz/superalloy/ServletConfigDemoServlet.java
@@ -0,0 +1,63 @@
+package biz.superalloy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "ServletConfigDemoServlet", 
+	urlPatterns = { "/servletConfigDemo" }, 
+	initParams = { 
+			@WebInitParam(name = "admin", value = "Harry Taciak"), 
+			@WebInitParam(name = "email", value = "admin@example.com") 
+		}
+	)
+public class ServletConfigDemoServlet implements Servlet {
+	
+	private transient ServletConfig servletConfig;
+	
+	@Override
+	public void init(ServletConfig servletConfig){
+		this.servletConfig = servletConfig;
+	}
+	
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException ,IOException {
+		ServletConfig servletConfig = getServletConfig();
+		String admin = servletConfig.getInitParameter("admin");
+		String email = servletConfig.getInitParameter("email");
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.print("<html><head></head><body>" + "Admin:" + admin + "<br/>Email:" + email + "</body></html>");
+		
+		ServletContext servletContext = servletConfig.getServletContext();
+		Enumeration<String> enumeration = servletContext.getAttributeNames();
+		while(enumeration.hasMoreElements()){
+			System.out.println(enumeration.nextElement());
+		}
+	}
+
+	@Override
+	public ServletConfig getServletConfig(){
+		return servletConfig;
+	}
+	
+	@Override
+	public String getServletInfo(){
+		return "ServletConfig demo";
+	}
+	
+	@Override
+	public void destroy(){
+		
+	}	
+
+}
